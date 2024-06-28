d6e08eda0f74984c1e8b3ca87b0fa1713db0f4c5
hanyiseul <seulli_hys@daum.net>
2024-06-28 16:15:59 +0900

    글쓰기 작업

--- carrot-app/yiseul/src/components/Input.jsx
+++ carrot-app/yiseul/src/components/Input.jsx
@@ -1,22 +1,35 @@
 import PropTypes from 'prop-types'; // PropTypes 불러오기
+import { useState } from 'react';
+import Image from './Image';
 
-export default function Input({label, value, placeholder, readonly, disabled, accept, onChange, multiple, uploadImgUrl, type='text'}) {
+export default function Input({label, value, placeholder, readonly, disabled, accept, onChange, multiple, type='text'}) {
+  const [uploadImgUrl, setUploadImgUrl] = useState([]);
+
+  const onchangeImageUpload = (e) => {
+    console.log(e.target.files)
+    const imageList = e.target.files; // 업로드된 파일 리스트를 가져옴
+    let imgUrlList = [...uploadImgUrl]; // 기존의 이미지 URL 리스트를 복사
+    for (let i = 0; i < imageList.length; i++) { // 파일 리스트를 순회하며
+      const imgUrl = URL.createObjectURL(imageList[i]); // 각 파일의 URL을 생성
+      imgUrlList.push(imgUrl); // 생성된 URL을 리스트에 추가
+    }
+    setUploadImgUrl(imgUrlList); // 업데이트된 URL 리스트를 상태로 설정
+  }
+
   return (
     <div className="input__wrap">
       {label && (
         <span className="label"> {label} </span>
       )}
       {uploadImgUrl && uploadImgUrl.map((img, id) => ( // 수정된 부분: 이미지를 렌더링하는 코드 추가
-        <div className="img" key={id}>
-          <img src={img} alt="" />
-        </div>
+        <Image id={id} img={img}/>
       ))}
-      <input className={`input`}type={type} value={value} accept={accept} placeholder={placeholder} readOnly={readonly} disabled={disabled} onChange={onChange} multiple={multiple}/>
+      <input className={`input`}type={type} value={value} accept={accept} placeholder={placeholder} readOnly={readonly} disabled={disabled} onChange={multiple ? onchangeImageUpload : onChange} multiple={multiple}/>
     </div>
   )
 }
 
-// PropTypes 추가ㅇ
+// PropTypes 추가
 Input.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
--- carrot-app/yiseul/src/pages/ProductWrite.jsx
+++ carrot-app/yiseul/src/pages/ProductWrite.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from '../components/Input'
 
 export default function ProductWrite() {
-  const [uploadImgUrl, setUploadImgUrl] = useState([]);
 
-  const onchangeImageUpload = (e)=> {
-    const imageList = e.target.files;
-    let imgUrlList = [...uploadImgUrl];
-    for (let i = 0; i < imageList.length; i++) {
-      const imgUrl = URL.createObjectURL(imageList[i]);
-      imgUrlList.push(imgUrl);
-    }
-    setUploadImgUrl(imgUrlList);
-  }
+  
   return (
     <form className="form">
-      <Input type="file" accept="image/*" multiple={true} onChange={onchangeImageUpload} uploadImgUrl={uploadImgUrl} />
+      <Input type="file" accept="image/*" multiple={true} />
       <Input label="제목" placeholder="글제목"/>
       <Input label="가격" type="number" />
-      
+      <Map />
     </form>
   )
 }
