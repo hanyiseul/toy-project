28d2cf5555b4dc8add0257a8613fab5d33631f02
hanyiseul <seulli_hys@daum.net>
2024-11-25 08:15:00 +0900

    코테 연습

---
+++ coding-test/yiseul/7-2.배열만들기2.js
@@ -0,0 +1,29 @@
+/****
+ * 
+ * 정수 l과 r이 주어졌을 때, l 이상 r이하의 정수 중에서 숫자 "0"과 "5"로만 이루어진 모든 정수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+ 만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
+ * 
+ * 제한사항
+  - 1 ≤ l ≤ r ≤ 1,000,000
+ * 
+ * 
+ * 
+ */
+
+
+function solution(l, r) {
+    var answer = [];
+    
+    for(let i = l; i <= r; i++) { // l 이상 r이하
+        if(/^[05]+$/.test(i.toString())) { // 숫자 "0"과 "5" 필터링
+        answer.push(i) // 배열에 저장
+        
+        answer.sort((a,b) => a-b)
+        }
+    } 
+     if (answer.length === 0) {
+        return [-1];
+    }
+    
+    return answer;
+}
