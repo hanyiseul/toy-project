88c77588115e01aba710ad9105ed00a1ee0aa0b1
hanyiseul <seulli_hys@daum.net>
2026-03-16 17:12:14 +0900

    api 테스트

--- Web-Trading-System/front-end/src/App.jsx
+++ Web-Trading-System/front-end/src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "@/components/Header/Header";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then(res => res.text())
+      .then(data => setMsg(data));
+  }, []);
+
   return (
     <>
       <Header/>
+      {msg}
     </>
   );
 };
