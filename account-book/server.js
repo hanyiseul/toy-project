7abc32e2f55b8291e202f6cf08879378c0bd52d7
hanyiseul <seulli_hys@daum.net>
2026-03-19 15:07:19 +0900

    서버 경로 설정

---
+++ account-book/server.js
@@ -0,0 +1,21 @@
+// server 실행 기능 구현
+const express = require("express"); // express 모듈
+const path =require("path"); // 경로 모듈
+const app = express(); // express 객체 생성
+const port = 3000;
+const route = require('./backend/router'); // route.js 불러오기
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(route);;
+
+// Express 미들웨어 설정
+// 모든 API 요청이 들어오기 전에 실행되는 전처리 단계
+// 서버 전체에 적용되어야 하기 때문에 server.js에서 실행
+app.use(express.json()); // express.json() : json 파일 읽게 해주는 설정
+
+// local 실행
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+    console.log("연결 성공")
+});
