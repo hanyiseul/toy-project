05aae36eb23a78d6fd28041f02d9bf47405f771d
hanyiseul <seulli_hys@daum.net>
2026-04-27 00:58:06 +0900

    필터탭 작업 예정

---
+++ react-todoList/client/src/pages/TodoList/TodoFilter.jsx
@@ -0,0 +1,18 @@
+{/* filter */}
+      <ul className="flex justify-between mt-6 gap-2">
+        <li className="flex-1">
+          <button className="w-full py-2 rounded-lg border bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white">
+            전체
+          </button>
+        </li>
+        <li className="flex-1">
+          <button className="w-full py-2 rounded-lg border border-gray-200 bg-white">
+            진행
+          </button>
+        </li>
+        <li className="flex-1">
+          <button className="w-full py-2 rounded-lg border border-gray-200 bg-white">
+            완료
+          </button>
+        </li>
+      </ul>
